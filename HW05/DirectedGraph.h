8ab4c45a8a968fc08d3a6aa3fb9e90c2e333b58f
nimrod46 <nimrodma99@gmail.com>
2023-01-26 19:40:21 +0200

    Added files read and user input

--- HW05/DirectedGraph.h
+++ HW05/DirectedGraph.h
@@ -7,14 +7,15 @@
 
 #include <vector>
 #include <ostream>
+#include <iostream>
 
 template<typename V>
 class DirectedGraph {
 
 private:
 
-    std::vector<std::vector<int> *> *matrix;
-    std::vector<V> *vertices;
+    std::vector<std::vector<int>> matrix;
+    std::vector<V> vertices;
 
 
 public:
@@ -32,9 +33,11 @@ public:
 
     double getEdge(V from, V to);
 
-    std::vector<V> *getNeighbourVertices(V v);
+    std::vector<V> getNeighbourVertices(V v);
 
-    std::vector<V> *getAllConnectedVertices(V v);
+    std::vector<V> getAllConnectedVertices(V v);
+
+    bool containsVertex(V v);
 
     void print();
 
@@ -43,27 +46,27 @@ public:
 
     int getVertexIndex(V v);
 
-    void getAllConnectedVertices(V v, std::vector<V> *connectedVertices);
+    void getAllConnectedVertices(V v, std::vector<V> connectedVertices);
 
     int getItemIndex(V v, typename std::vector<V>::iterator begin, typename std::vector<V>::iterator end);
 };
 
 template<typename V>
-DirectedGraph<V>::DirectedGraph(): matrix(new std::vector<std::vector<int> *>()), vertices(new std::vector<V>()) {
+DirectedGraph<V>::DirectedGraph(): matrix(), vertices() {
 
 }
 
 template<typename V>
 void DirectedGraph<V>::addVertex(V v) {
-    vertices->push_back(v);
-    std::vector<int> *row = new std::vector<int>();
-    for (int i = 0; i < matrix->size(); ++i) {
-        row->push_back(0);
+    if(containsVertex(v)) {
+        return;
     }
-    matrix->push_back(row);
+    vertices.push_back(v);
+    std::vector<int> row(matrix.size(), 0);
+    matrix.push_back(row);
 
-    for (std::vector<int> *i: *matrix) {
-        i->push_back(0);
+    for (std::vector<int>& i: matrix) {
+        i.push_back(0);
     }
 
 }
@@ -75,47 +78,45 @@ bool DirectedGraph<V>::removeVertex(V v) {
         return false;
     }
 
-    vertices->erase(vertices->begin() + index);
-    matrix->erase(matrix->begin() + index);
-    for (std::vector<int> *i: *matrix) {
-        i->erase(i->begin() + index);
+    vertices.erase(vertices.begin() + index);
+    matrix.erase(matrix.begin() + index);
+    for (std::vector<int> i: matrix) {
+        i.erase(i.begin() + index);
     }
 }
 
 template<typename V>
 void DirectedGraph<V>::addEdge(V from, V to, int weight) {
-    int fromIndex = getVertexIndex(from);
-    int toIndex = getVertexIndex(to);
-    matrix->at(fromIndex)->at(toIndex) = weight;
+    setEdge(from, to, weight);
 }
 
 template<typename V>
 void DirectedGraph<V>::removeEdge(V from, V to) {
-    int fromIndex = getVertexIndex(from);
-    int toIndex = getVertexIndex(to);
-    matrix->at(fromIndex)->at(toIndex) = 0;
+    setEdge(from, to, 0);
 }
 
 template<typename V>
 void DirectedGraph<V>::setEdge(V from, V to, int weight) {
-    addEdge(from, to, weight);
+    int fromIndex = getVertexIndex(from);
+    int toIndex = getVertexIndex(to);
+    matrix.at(fromIndex).at(toIndex) = weight;
 }
 
 template<typename V>
 double DirectedGraph<V>::getEdge(V from, V to) {
     int fromIndex = getVertexIndex(from);
     int toIndex = getVertexIndex(to);
-    return matrix->at(fromIndex)->at(toIndex);
+    return matrix[fromIndex][toIndex];
 }
 
 template<typename V>
-std::vector<V> *DirectedGraph<V>::getNeighbourVertices(V v) {
-    std::vector<V> *neighbourVertices = new std::vector<V>();
+std::vector<V> DirectedGraph<V>::getNeighbourVertices(V v) {
+    std::vector<V> neighbourVertices;
     int index = getVertexIndex(v);
 
-    for (int i = 0; i < matrix->size(); ++i) {
-        if (matrix->at(index)->at(i) != 0) {
-            neighbourVertices->push_back(vertices->at(i));
+    for (size_t i = 0; i < matrix.size(); ++i) {
+        if (matrix[index][i] != 0) {
+            neighbourVertices.push_back(vertices[i]);
         }
     }
 
@@ -123,10 +124,9 @@ std::vector<V> *DirectedGraph<V>::getNeighbourVertices(V v) {
 }
 
 template<typename V>
-std::vector<V> *DirectedGraph<V>::getAllConnectedVertices(V v) {
-    std::vector<V> *connectedVertices = new std::vector<V>();
+std::vector<V> DirectedGraph<V>::getAllConnectedVertices(V v) {
+    std::vector<V> connectedVertices;
     connectedVertices->push_back(v);
-    int index = getVertexIndex(v);
 
     getAllConnectedVertices(v, connectedVertices);
 
@@ -135,32 +135,45 @@ std::vector<V> *DirectedGraph<V>::getAllConnectedVertices(V v) {
 }
 
 template<typename V>
-void DirectedGraph<V>::getAllConnectedVertices(V v, std::vector<V> *connectedVertices) {
-    if (getItemIndex(v, connectedVertices->begin(), connectedVertices->begin()) != -1) {
+void DirectedGraph<V>::getAllConnectedVertices(V v, std::vector<V> connectedVertices) {
+    if (getItemIndex(v, connectedVertices.begin(), connectedVertices.begin()) != -1) {
         return;
     }
 
     int index = getVertexIndex(v);
-    std::vector<V> *neighbourVertices = getNeighbourVertices(v);
-    for (V u: *neighbourVertices) {
-        connectedVertices->push_back(u);
+    std::vector<V> neighbourVertices = getNeighbourVertices(v);
+    for (V u: neighbourVertices) {
+        connectedVertices.push_back(u);
         getAllConnectedVertices(u, connectedVertices);
     }
 }
 
 template<typename V>
 void DirectedGraph<V>::print() {
+    std::cout << *this;
+}
 
+template<typename V>
+bool DirectedGraph<V>::containsVertex(V v) {
+    return getVertexIndex(v) != -1;
 }
 
 template<class U>
-std::ostream &operator<<(std::ostream &stream, DirectedGraph<U> &queue) {
+std::ostream &operator<<(std::ostream &stream, DirectedGraph<U> &directedGraph) {
+    for (size_t i = 0; i < directedGraph.matrix.size(); ++i) {
+        stream << directedGraph.vertices[i] << ": ";
+        std::vector<U> neighbourVertices = directedGraph.getNeighbourVertices(directedGraph.vertices[i]);
+        for (U u : neighbourVertices) {
+            stream << u << " ";
+        }
+        stream << std::endl;
+    }
     return stream;
 }
 
 template<typename V>
 int DirectedGraph<V>::getVertexIndex(V v) {
-    return getItemIndex(v, vertices->begin(), vertices->end());
+    return getItemIndex(v, vertices.begin(), vertices.end());
 }
 
 template<typename V>
--- HW05/main.cpp
+++ HW05/main.cpp
@@ -1,14 +1,18 @@
+#include <fstream>
+#include <sstream>
 #include "DirectedGraph.h"
+
+void userInput(DirectedGraph<std::string> &graph);
 
 //
 // Created by nimrod on 26/01/2023.
 //
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     std::vector<std::string> filesNames;
     std::string outputFileName = "output.dat";
     for (int i = 1; i < argc; ++i) {
         std::string arg = argv[i];
-        if(arg == "-o") {
+        if (arg == "-o") {
             outputFileName = argv[i + 1];
             i++;
             continue;
@@ -16,10 +20,68 @@ int main(int argc, char* argv[]) {
         filesNames.emplace_back(arg);
     }
 
-    
-
     DirectedGraph<std::string> dg;
 
+    std::ifstream infile;
+    for (const std::string &fileName: filesNames) {
+        infile.open(fileName);
+        std::string data;
+        while (std::getline(infile, data)) {
+//            std::cout << data << "end of line" << std::endl;
+//            std::cout.flush();
+
+            std::stringstream line(data);
+            std::string from;
+            line >> from;
+
+            std::string to;
+            line >> to;
+
+            int weight;
+            line >> weight;
+
+            dg.addVertex(from);
+            dg.addVertex(to);
+            dg.addEdge(from, to, weight);
+        }
+        infile.close();
+    }
+
+
+    std::ofstream file;
+    file.open(outputFileName);
+    file << dg;
+
+    userInput(dg);
 
     return 0;
+}
+
+void userInput(DirectedGraph<std::string> &graph) {
+
+    while (true) {
+        std::string sourceNode;
+        std::cin >> sourceNode;
+        if (sourceNode == "exit") {
+            return;
+        }
+
+        if (!graph.containsVertex(sourceNode)) {
+
+            std::cout << sourceNode << " does not exist in the current network" << std::endl;
+            std::cout << "USAGE: <node> or ‘exit’ to terminate" << std::endl;
+            continue;
+        }
+
+        std::vector<std::string> connectedNodes = graph.getNeighbourVertices(sourceNode);
+        if (connectedNodes.empty()) {
+            std::cout << sourceNode << " : " << "no outbound travel" << std::endl;
+            continue;
+        }
+        std::cout << sourceNode;
+        for (const std::string &node: connectedNodes) {
+            std::cout << '\t' << node;
+        }
+        std::cout << std::endl;
+    }
 }
